b5353a8b51097838abf6de5f0547a83a739bcded
TheBossMagnus <TheBossMagnus@proton.me>
2025-08-18 21:32:00 +0200

    fix: fix the modmenu dep

--- fabric/src/main/java/io/github/thebossmagnus/mods/config_manager/fabric/client/ConfigManagerFabricClient.java
+++ fabric/src/main/java/io/github/thebossmagnus/mods/config_manager/fabric/client/ConfigManagerFabricClient.java
@@ -1,11 +1,8 @@
 package io.github.thebossmagnus.mods.config_manager.fabric.client;
 
 
-import com.mojang.authlib.minecraft.client.MinecraftClient;
 import io.github.thebossmagnus.mods.config_manager.common.Loaded;
-import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.gui.screens.Screen;
 
 
 public final class ConfigManagerFabricClient implements ClientModInitializer {
